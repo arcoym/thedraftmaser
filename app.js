aecde6a5bdcf0f5995f378de4092acabbb4e2694
Alexandra Coym <arc507@nyu.edu>
2014-05-05 19:21:19 -0400

    website structure added + math on server

--- app.js
+++ app.js
@@ -7,15 +7,31 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var request = require('request'); // library to make requests to remote urls
+var ejs = require('ejs');
 
-
-var app = module.exports = express();
 
 // the ExpressJS App
-var app = express();
+var app = express()
+
 var newValue;
 var tempValue;
+var idealTemp;
+var angle;
+var outsideTempF = 75;
+var outsideTempC = 22;
+var tempDiff = 0;
+
+// Setup Variables
+var name;
+var zipcode;
+var scale;
+var tempRange;
+var allergies;
 	
+  //app.use('/views', express.static(__dirname + '/views')); 
+  //app.set('/views', __dirname + '/views');
+
+
 app.configure( function() { 
 	/*
 	   use a directory called js in your project directory
@@ -27,7 +43,12 @@ app.configure( function() {
 	   will then serve files from those directories like a regular
 	   webserver.
 	*/
+
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
   app.use('/js', express.static(__dirname + '/js')); 
+  app.use(express.bodyParser());
+
   console.log("Express configured. Listening on port 8080");
 });
 
@@ -53,17 +74,11 @@ app.get('/state', function (req, res) {
 });
 
 app.get('/temp/:value', function (req, res) {
-	tempValue = req.params.value;
-
-	//DO TEMPERATURE MATH HERE
-	
+	tempValue = req.params.value;	
 });
+
 app.get('/angle/:value', function (req, res) {
-	
 	currentAngle = req.params.value;
-
-	//DO ANGLE MATH HERE
-
 });
 
 // function for serving index.html, or index. anything:
@@ -77,6 +92,295 @@ app.get('/', function (req, res) {
    res.sendfile('index.html');
 });
 
+//ROUTING OF SETUP PAGES
+app.get('/setup', function (req, res) {
+   res.sendfile('setup1.html');
+});
+
+// INITIAL SETUP OF DEVICE - saving variables below goes here
+app.get('/ready', function (req, res) {
+		res.sendfile('setup2.html');
+});
+app.get('/later', function (req, res) {
+		res.sendfile('later.html');
+});
+
+
+app.post('/setup2', function (req, res) {
+	nameNew = req.body.name;
+	console.log(nameNew);
+	//res.sendfile('setup3.html')
+	res.render('setup3', { 
+		name: nameNew 
+	}, function (err,data) {
+		if(!err) {	
+			res.write(data);
+			res.end();
+		}
+		console.log(err);
+		});
+});
+
+app.post('/setup3', function (req, res) {
+	zipcode = req.body.zipcode;
+	console.log(zipcode);
+	res.sendfile('setup4.html');
+});
+
+app.post('/setup4', function (req, res) {
+	scale = req.body.scale;
+
+	//celsius scale
+	63-67
+	68-72
+	73-77
+
+	//fahrenheit scale
+	17-19
+	20-22
+	24-26
+
+	if(scale == 0) {
+		//render next page with C values
+		res.render('setup5.ejs', {
+			range1: '17-19',
+			range2: '20-22',
+			range3: '24-26'
+		});
+	};
+
+	if(scale == 1) {
+		//render next page with F values
+		res.render('setup5.ejs', {	
+			range1: "63-67",
+			range2: "68-72",
+			range3: "73-77"
+		});
+	};
+});
+
+app.post('/setup5', function (req, res) {
+	tempRange = req.body.range;
+	console.log(tempRange);
+
+	res.sendfile('setup6.html');
+});
+
+app.post('/setupfinish', function (req, res) {
+	allergies = req.body.allergies;
+	console.log(allergies);
+	res.sendfile('setupfinish.html');
+});
+
+
+//BIG FUNCTION THAT DOES EVERYTHING HERE
+var math = funtion() {
+
+	if(tempValue == idealTemp) {
+		// do nothing		
+	}
+
+	//set idealTemp for CELSIUS
+	if(scale == 0) {
+		if(tempRange == 1) {
+			idealTemp = 15;
+		}
+		if(tempRange ==2) {
+			idealTemp = 18;
+		}
+		if(tempRange ==3) {
+			idealTemp = 21;
+		}
+		if(tempRange = 4) {
+			idealTemp = 25;
+		}
+		if(tempRange ==5) {
+			idealTemp = 28;
+		}
+		if(tempRange ==6) {
+			idealTemp = 21;
+		}
+	}
+
+	//set idealTemp for CELSIUS
+	if(scale == 1) {
+		if(tempRange == 1) {
+			idealTemp = 55;
+		}
+		if(tempRange ==2) {
+			idealTemp = 65;
+		}
+		if(tempRange ==3) {
+			idealTemp = 70;
+		}
+		if(tempRange = 4) {
+			idealTemp = 75;
+		}
+		if(tempRange ==5) {
+			idealTemp = 85;
+		}
+		if(tempRange ==6) {
+			idealTemp = 70;
+		}
+	}
+	else {
+		console.log('something is up');
+	}
+
+
+	if (scale == 0) {
+
+		if(tempValue > idealTemp) {
+			 tempDiff= tempValue-idealTemp;
+			if(outsideTempC < idealTemp) {
+			
+			var tempChange = idealTemp - outsideTempC;
+
+				if(tempChange <= 2) {
+					angle = 2;
+				}
+				if(tempChange >= 3 || tempChange <=5) {
+					angle = 3;
+				}
+				if(tempChange >= 6) {
+					angle = 4;
+				}
+			}	
+		} 
+
+		if(tempValue > idealTemp) {
+			tempDiff = tempValue-idealTemp;
+			if(outsideTempC > idealTemp) {
+			
+			var tempChange =  outsideTempC - idealTemp;
+				
+				if(tempChange <= 3) {
+					angle = 2;
+				}
+				if(tempChange >= 4 || tempChange <=8) {
+					angle = 3;
+				}
+				if(tempChange >= 9) {
+					angle = 4;
+				}
+			}	
+		} 
+
+		if(tempValue < idealTemp) {
+			tempDiff = idealTemp - tempValue;
+			
+			if(outsideTempC < idealTemp) {
+			
+			var tempChange = idealTemp - outsideTempC;
+
+				if(tempChange <= 2) {
+					angle = 2;
+				}
+				if(tempChange >= 3 || tempChange <=7) {
+					angle = 3;
+				}
+				if(tempChange >= 8) {
+					angle = 4;		
+				}
+			}	
+		}
+
+		if(tempValue < idealTemp) {
+			tempDiff = idealTemp - temValue;
+			
+			if(outsideTempC > idealTemp) {
+			
+			var tempChange = outsideTempC - idealTemp;
+				if(tempChange <= 2) {
+					angle = 2;
+				}
+				if(tempChange >= 3 || tempChange <=7) {
+					angle = 3;
+				}
+				if(tempChange >= 8) {
+					angle = 4;
+				}			
+			}	
+		}
+	}
+
+	// FOR FAHRENHEIT
+
+	if (scale == 1) {
+		
+		//C to F math =	 Multiply by 9, then divide by 5, then add 32
+		tempValueF = ((tempValue * 9) / 5) + 32;
+
+		if(tempValueF > idealTemp) {
+			tempDiff = tempValueF-idealTemp;
+			
+			if(outsideTempF < idealTemp) {
+			var tempChange = idealTemp - outsideTempF;
+				if(tempChange <= 2) {
+					angle = 2;
+				}
+				if(tempChange >= 3 || tempChange <=7) {
+					angle = 3;
+				}
+				if(tempChange >= 8) {
+					angle = 4;
+			
+				}
+			}	
+		} 
+
+		if(tempValueF > idealTemp) {
+			tempDiff = tempValueF-idealTemp;
+			
+			if(outsideTempF > idealTemp) {
+			var tempChange =  outsideTempF - idealTemp;
+				if(tempChange <= 2) {
+					angle = 2;
+				}
+				if(tempChange >= 3 || tempChange <=7) {
+					angle = 3;
+				}
+				if(tempChange >= 8) {
+					angle = 4;
+				}			
+			}
+		}	
+
+		if(tempValueF < idealTemp) {
+			tempDiff = idealTemp - tempValueF;
+			
+			if(outsideTempF < idealTemp) {
+			var tempChange = idealTemp - outsideTempF;
+				if(tempChange <= 2) {
+					angle = 2;
+				}
+				if(tempChange >= 3 || tempChange <=7) {
+					angle = 3;
+				}
+				if(tempChange >= 8) {
+					angle = 4;
+				}			
+			}	
+		}
+
+		if(tempValueF < idealTemp) {
+			tempDiff = idealTemp - temValue;
+			
+			if(outsideTempF > idealTemp) {
+			var tempChange = outsideTempF - idealTemp;
+				if(tempChange <= 2) {
+					angle = 2;
+				}
+				if(tempChange >= 3 || tempChange <=7) {
+					angle = 3;
+				}
+				if(tempChange >= 8) {
+					angle = 4;
+				}
+			}	
+		}
+	}
+}
 
 // start listening on port 8080:
 app.listen(8080);
